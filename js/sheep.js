4661f54ac486a75d0940653edd4219d05b8e7b46
Lorien Hu <lorienbh@gmail.com>
2015-01-25 00:26:53 -0800

    not quite working sheep movement, but sheep now follow new coordinate system

--- js/sheep.js
+++ js/sheep.js
@@ -1,5 +1,5 @@
 var Sheep = function() {
-
+    this.name = "Sheep";
     this.spriteSheet = new createjs.SpriteSheet({
       images: ["assets/img/sheep.png"],
       frames: {width: 40, height: 40, regX: 20, regY: 20},
@@ -19,7 +19,7 @@ var Sheep = function() {
     this.tileX = 5;
     this.tileY = 5;
 
-    this.images = ["img/dog.png"];
+    this.images = ["img/sheep.png"];
     var sheepDir = 3; // 2 left, 6 right, 0 up, 4 down, 5 SE, 3 SW, 7 NE, 1 NW 
 
     this.getWorldX = function() {
@@ -37,22 +37,22 @@ var Sheep = function() {
   };
 
 var Flock = function(NumSheep) {
-  var sheepFlock = [];
-  var sheepState = "walking";
+  this.sheepFlock = [];
+  this.sheepState = "walking";
   var randomSheep = 0;
   var moveSheep = 0;
 
 
     for (i=0; i<NumSheep; i++) {
       var daSheep = new Sheep(); 
-      sheepFlock.push(daSheep);
+      this.sheepFlock.push(daSheep);
     }
 
   this.randomizeSheep = function () {
-    if (sheepState == "walking") {
+    if (this.sheepState == "walking") {
         randomSheep++;
         if (randomSheep>10) {
-        var chosenOne = sheepFlock[(Math.floor((Math.random() * sheepFlock.length) + 0))]; 
+        var chosenOne = this.sheepFlock[(Math.floor((Math.random() * this.sheepFlock.length) + 0))]; 
         chosenOne.sheepDir = (Math.floor((Math.random() * 7) + 0));
         chosenOne.sprite.gotoAndStop(chosenOne.sheepDir);
         randomSheep = 0;
@@ -61,7 +61,7 @@ var Flock = function(NumSheep) {
   };
 // returnes the array of sheep
   this.getFlock = function() {
-    return sheepFlock;
+    return this.sheepFlock;
   };
 
   this.moveFlock = function() {
@@ -69,52 +69,53 @@ var Flock = function(NumSheep) {
     var targetX = this.tileX;
     var targetY = this.tileY;
 
-    for (i = 0; i<sheepFlock.length; i++) {
+    for (i = 0; i<this.sheepFlock.length; i++) {
       // ugly but works
-      if (sheepFlock[i].sheepDir == 0){
+      if (this.sheepFlock[i].sheepDir == 0){
           targetX -= 2;
         }
-      if (sheepFlock[i].sheepDir == 3){
+      if (this.sheepFlock[i].sheepDir == 3){
           targetX += 2;
         }
-      if (sheepFlock[i].sheepDir == 2){
+      if (this.sheepFlock[i].sheepDir == 2){
           targetY += 2;
         }
-      if (sheepFlock[i].sheepDir == 1){
+      if (this.sheepFlock[i].sheepDir == 1){
           targetY -= 2;
         }
 
-      if (sheepFlock[i].sheepDir == 4){
+      if (this.sheepFlock[i].sheepDir == 4){
           targetY -= 2;
           targetX += 2;
-          sheepFlock[i].sprite.gotoAndStop(3);
+          this.sheepFlock[i].sprite.gotoAndStop(3);
         }
 
-      if (sheepFlock[i].sheepDir == 5){
+      if (this.sheepFlock[i].sheepDir == 5){
           targetY -= 2;
           targetX -= 2;
 
         }
 
-      if (sheepFlock[i].sheepDir == 6){
+      if (this.sheepFlock[i].sheepDir == 6){
           targetY += 2;
           targetX += 2;
 
         }
 
-      if (sheepFlock[i].sheepDir == 7){
+      if (this.sheepFlock[i].sheepDir == 7){
           targetY += 2;
           targetX -= 2;
-
       }
 
       if (isValidDirection(targetX, targetY)) {
+
+        console.log("AH GOT HERE" + this.name);
         this.tileX = targetX;
         this.tileY = targetY;
       }
     }
-     this.randomizeSheep();
-     moveSheep = 0; 
+    this.randomizeSheep();
+    moveSheep = 0; 
   };
 };
 
