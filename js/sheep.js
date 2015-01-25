5a6266e81469c97e0e95b377dd32cefaeb487b0b
Shakthi Wijeratne <shakthiw@gmail.com>
2015-01-25 01:23:21 -0800

    fixed sheep moving

--- js/sheep.js
+++ js/sheep.js
@@ -20,7 +20,25 @@ var Sheep = function() {
     this.tileY = 5;
 
     this.images = ["img/sheep.png"];
-    var sheepDir = 3; // 2 left, 6 right, 0 up, 4 down, 5 SE, 3 SW, 7 NE, 1 NW 
+    this.sheepDir = 3; // 2 left, 6 right, 0 up, 4 down, 5 SE, 3 SW, 7 NE, 1 NW 
+    this.state = "walking";
+    this.grazeTime = 0;
+
+    this.changeState = function() {
+      if (this.state == "walking") {
+          
+      }
+      else {
+        if (this.state == "scared") {
+
+        }
+      }
+      else {
+        if (this.state == "grazing") {
+
+        } 
+      }
+    };
 
     this.getWorldX = function() {
       return isoToWorld(this.tileX, this.tileY)[0];
@@ -29,34 +47,25 @@ var Sheep = function() {
     this.getWorldY = function() {
       return isoToWorld(this.tileX, this.tileY)[1];
     }
-
-    this.sheepDistance = function () {
-      
-    }
-
   };
 
 var Flock = function(NumSheep) {
   this.sheepFlock = [];
-  this.sheepState = "walking";
   var randomSheep = 0;
   var moveSheep = 0;
 
 
-    for (i=0; i<NumSheep; i++) {
+    for (var i=0; i<NumSheep; i++) {
       var daSheep = new Sheep(); 
       this.sheepFlock.push(daSheep);
     }
 
   this.randomizeSheep = function () {
-    if (this.sheepState == "walking") {
         randomSheep++;
         if (randomSheep>10) {
         var chosenOne = this.sheepFlock[(Math.floor((Math.random() * this.sheepFlock.length) + 0))]; 
         chosenOne.sheepDir = (Math.floor((Math.random() * 7) + 0));
-        chosenOne.sprite.gotoAndStop(chosenOne.sheepDir);
         randomSheep = 0;
-      }
     }
   };
 // returnes the array of sheep
@@ -68,40 +77,47 @@ var Flock = function(NumSheep) {
 
     var sheepSpeed = 0.01;
 
-    for (i = 0; i<this.sheepFlock.length; i++) {
+    for (var i = 0; i<this.sheepFlock.length; i++) {
 
       var targetX = this.sheepFlock[i].tileX;
       var targetY = this.sheepFlock[i].tileY;
 
       // ugly but works
-      if (this.sheepFlock[i].sheepDir == 0){
+      if (this.sheepFlock[i].sheepDir == 1){
           targetX -= sheepSpeed;
+          this.sheepFlock[i].sprite.gotoAndStop(1);
+        }
+      else if (this.sheepFlock[i].sheepDir == 5){
+          targetX += sheepSpeed;
+          this.sheepFlock[i].sprite.gotoAndStop(5);
+        }
+      else if (this.sheepFlock[i].sheepDir == 7){
+          targetY -= sheepSpeed;
+          this.sheepFlock[i].sprite.gotoAndStop(7);
         }
       else if (this.sheepFlock[i].sheepDir == 3){
+          targetY += sheepSpeed;
+          this.sheepFlock[i].sprite.gotoAndStop(3);
+        }
+      else if (this.sheepFlock[i].sheepDir == 4){
+          targetY += sheepSpeed;
           targetX += sheepSpeed;
+          this.sheepFlock[i].sprite.gotoAndStop(4);
         }
       else if (this.sheepFlock[i].sheepDir == 2){
           targetY += sheepSpeed;
-        }
-      else if (this.sheepFlock[i].sheepDir == 1){
-          targetY -= sheepSpeed;
-        }
-      else if (this.sheepFlock[i].sheepDir == 4){
-          targetY -= sheepSpeed;
-          targetX += sheepSpeed;
-          this.sheepFlock[i].sprite.gotoAndStop(3);
-        }
-      else if (this.sheepFlock[i].sheepDir == 5){
-          targetY -= sheepSpeed;
           targetX -= sheepSpeed;
+          this.sheepFlock[i].sprite.gotoAndStop(2);
         }
       else if (this.sheepFlock[i].sheepDir == 6){
-          targetY += sheepSpeed;
+          targetY -= sheepSpeed;
           targetX += sheepSpeed;
+          this.sheepFlock[i].sprite.gotoAndStop(6);
         }
-      else if (this.sheepFlock[i].sheepDir == 7){
-          targetY += sheepSpeed;
+      else if (this.sheepFlock[i].sheepDir == 0){
+          targetY -= sheepSpeed;
           targetX -= sheepSpeed;
+          this.sheepFlock[i].sprite.gotoAndStop(0);
       }
 
       if (this.sheepFlock[i].name == "Sheep") {
